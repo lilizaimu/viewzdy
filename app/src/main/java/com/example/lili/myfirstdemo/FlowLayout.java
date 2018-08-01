4d4c59762d4021344baff51b5df296ffe3122c74
lili <shandongqdlpl@163.com>
2018-08-01 19:19:52 +0800

    flowlayout

---
+++ app/src/main/java/com/example/lili/myfirstdemo/FlowLayout.java
@@ -0,0 +1,36 @@
+package com.example.lili.myfirstdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 13096 on 2018/7/30.
+ */
+
+public class FlowLayout extends ViewGroup {
+
+
+    ArrayList<View> views;
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new ViewGroup.MarginLayoutParams(getContext(), attrs);
+    }
+}
