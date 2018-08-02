fd6bd305690f5f6fc8b2d9938305d9c7fe1c8967
lili <shandongqdlpl@163.com>
2018-08-02 20:58:49 +0800

    flowlayout

--- app/src/main/java/com/example/lili/myfirstdemo/FlowLayout.java
+++ app/src/main/java/com/example/lili/myfirstdemo/FlowLayout.java
@@ -16,9 +16,6 @@ public class FlowLayout extends ViewGroup {
 
     ArrayList<View> views;
 
-    float maxLineWidth;
-    float maxLineHeight;
-
     public FlowLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -34,15 +31,32 @@ public class FlowLayout extends ViewGroup {
 
         int count = getChildCount();
 
-        float lineHeight;
-        float lineWidth;
+        float maxWidth = 0;
+        float allHeight = 0;
+        float lineHeight = 0;
+        float lineWidth = 0;
         for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
             measureChild(child, widthMeasureSpec, heightMeasureSpec);
-            MarginLayoutParams layoutParams= (MarginLayoutParams) child.getLayoutParams();
-            
-        }
+            MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+            int childWidth = layoutParams.width;
+            int childHeight = layoutParams.height;
 
+            if (lineWidth + childWidth < widthSize) {
+                lineHeight = Math.max(lineHeight, childHeight);
+                lineWidth = childWidth + lineWidth;
+            } else {
+                maxWidth = Math.max(lineWidth, maxWidth);
+                allHeight += lineHeight;
+                lineHeight = 0;
+                lineWidth = 0;
+            }
+            if (i == count - 1) {  //如果是最后一个
+                lineHeight = Math.max(lineHeight, childHeight);
+                allHeight += lineHeight;
+                maxWidth = Math.max(maxWidth, childWidth);
+            }
+        }
     }
 
     @Override
